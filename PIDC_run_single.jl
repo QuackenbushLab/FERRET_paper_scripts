09f95cd5e51a2b4b22b60ecdd343da5bb8078349
Tara Eicher <taraeicher@yahoo.com>
2025-03-04 21:59:55 -0500

    Added PIDC script

--- PIDC_run_single.jl
+++ PIDC_run_single.jl
@@ -2,12 +2,15 @@ using NetworkInference
 using LightGraphs
 using DelimitedFiles
 
-dataset_name = string("/home/ubuntu/TCGA_single_cell_splits_pca/",ARGS[1])
+dataset_name = ARGS[1]
 algorithm = PIDCNetworkInference()
 threshold = parse(Float64, ARGS[2])
+println(dataset_name)
 @time genes = get_nodes(dataset_name);
 @time network = InferredNetwork(algorithm, genes);
 adjacency_matrix, labels_to_ids, ids_to_labels = get_adjacency_matrix(network, threshold)
 graph = LightGraphs.SimpleGraphs.SimpleGraph(adjacency_matrix)
 adjacency_list = LightGraphs.SimpleGraphs.SimpleGraphs.adj(graph)
-writedlm(string("/home/ubuntu/PIDC_CPTAC/",ARGS[2], "_", ARGS[1]),adjacency_matrix, ',')
+println(labels_to_ids)
+println(ids_to_labels)
+writedlm(ARGS[3],adjacency_matrix, ',')
